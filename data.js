e8343ef05880a71d62c2ec7ac98afc5cd531cc80
James Frisella <jafrisella@gmail.com>
2017-04-07 09:49:34 -0400

    testing and getting to work on basic data

---
+++ data.js
@@ -0,0 +1,23 @@
+var ccData = [
+  {
+    "displayOrder": 1,
+    "timeRaw": "00:00:02,000 --> 00:00:04,000",
+    "begin": 2000,
+    "end": 4000,
+    "text": "Hi, my name is Rachelle Babler."
+  },
+  {
+    "displayOrder": 2,
+    "timeRaw": "00:00:04,000 --> 00:00:06,000",
+    "begin": 4000,
+    "end": 6000,
+    "text": "Howdy, this is the second section."
+  },
+  {
+    "displayOrder": 3,
+    "timeRaw": "00:00:08,000 --> 00:00:10,000",
+    "begin": 8000,
+    "end": 10000,
+    "text": "And this is a delayed one"
+  }
+]
--- jquery.closed-captioner.js
+++ jquery.closed-captioner.js
@@ -9,48 +9,86 @@
         var _options = {},
             _private = {};
         var _defaults = {
+
+            //in milliseconds
             time: 0,
-            data: NULL,
+
+            //json object
+            data: null,
+
+            //adds captions to dom
             render: function($el, sectionData){
-                $el.html("<p class='cc-caption'>" + section.text + "</p>");
+                console.log("options.render");
+                console.log(sectionData);
+                if(!sectionData){
+                    $el.html("");
+                }else{
+                    console.log($el);
+                    $el.html("<p class='cc-caption'>" + sectionData.text + "</p>");
+                }
             }
         };
 
 
-        //Private
-        _private.currentBeginTime = NULL;
-        _private.currentEndTime = NULL;
+        /**
+        *   Section is one of the individual json objects
+        *   when it is set, we use the beginTime and endTime on the object
+        *   to determine display.  When null, we search for the next one.
+        */
+        _private.section = null;
+        _private.sectionChange = 0;
+        _private.$el = null;
 
         /**
         *   Reset defaults
         */
         _private.reset = function(){
-            _options = NULL;
+            _options = null;
             _options = {};
-            _private.currentBeginTime = NULL;
-            _private.currentEndTime = NULL;
-        }
-
-        /**
-        *   Start everything
-        *     - ajax or data
-        */
-        _private.start = function(){
-
+            _private.section = null;
+            _private.$el = null;
         }
 
         /**
         *   Check current caption time
         */
         _private.checkTime = function(){
+            console.log("checkTime");
+            if(!_private.section || (_options.time < _private.section.begin || _options.time >= _private.section.end) ){
+                //Need to find the next section if there is one
+                _private.section = _private.getSection();
+                _private.sectionChange = 1;
+            }
+            //if time is in section, then nothing has changed
+            else if(_options.time >= _private.section.begin && _options.time < _private.section.end){
+                _private.sectionChange = 0;
+            }
 
+            _private.render();
+        }
+
+        /**
+        *   Determine whether to render caption or not
+        *   based on sectionChange
+        */
+        _private.render = function(){
+            console.log("render");
+            if(_private.sectionChange !== 1) return;
+            _private.sectionChange = 0;
+            _options.render(_private.$el, _private.section);
         }
 
         /**
         *   Get current caption section
         */
         _private.getSection = function(){
-
+            console.log("getSection");
+            for(var i = 0; i < _options.data.length; i+=1){
+                if(_options.time >= _options.data[i].begin && _options.time < _options.data[i].end){
+                    return _options.data[i];
+                }
+            }
+            return null;
         }
 
 
@@ -66,19 +104,29 @@
 
                 _private.reset();
 
+                //set element
+                _private.$el = $(this);
+
                 //extend defaults with options
                 _options = {};
                 $.extend(_options, _defaults, options);
 
-                _private.start();
+                //Do some data checks
+                if(!_options.data || typeof _options.data !== "object" || _options.data.length === 0){
+                  $.error("init : data : caption data is required");
+                  return false;
+                }
+
+                _private.checkTime();
             },
 
             /**
-            *   Update time in seconds
+            *   Update time in milliseconds
             */
-            updateTime: function(seconds){
-                _currentTime = seconds;
-
+            updateTime: function(ms){
+                console.log("updateTime");
+                console.log(ms);
+                _options.time = ms;
                 _private.checkTime();
             }
         };
